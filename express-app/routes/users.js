e64fba069dd8e023dcd14c01f52b6fb98aacbd9a
Weller26 <Ya.zloy246@gmail.com>
2026-03-24 17:24:14 +0500

    Add sqlite

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,30 +3,37 @@ const router = express.Router();
 
 /* GET users listing. */
 
-items = [
-  {"id": 1, "name": "Алсу"},
-  {"id": 2, "name": "Илья"},
-]
-let LAST_ID = 2;
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('mydb.db');
+db.run(`CREATE TABLE IF NOT EXISTS users (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   name text)`);
+
 router.get('/', function(req, res, next) {
-  res.status(200).json(items);
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(rows);
+    }
+  });
 });
 
 router.post('/', function(req, res) {
-  LAST_ID++;
-  const newUser = {"id": LAST_ID, "name": req.body.name};
-  items.push(req.body);
-  res.status(201).json(newUser);
+  const name = req.body.name;
+  const insert = "INSERT INTO users (name) VALUES (?)";
+  db.run(insert, [name]);
+  res.status(201).json({"message": "User was created"});
 })
 
 router.get('/:id', function(req, res, next) {
-  const targetId = Number(req.params.id);
-  const user = items.filter(item => item.id === targetId);
-  if (user.length === 0) {
-    res.status(404).json({"message": "User not found"});
-  } else {
-    res.status(200).json(user);
-  }
+  // const targetId = Number(req.params.id);
+  // const user = items.filter(item => item.id === targetId);
+  // if (user.length === 0) {
+  //   res.status(404).json({"message": "User not found"});
+  // } else {
+  //   res.status(200).json(user);
+  // }
 })
 
 module.exports = router;
