a1a2aac614806bde01e30dc40f320e79c1c13aea
Алсу Исламова <161420770+mrrled@users.noreply.github.com>
2026-03-24 17:04:55 +0500

    add post

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -7,8 +7,16 @@ items = [
   {"id": 1, "name": "Алсу"},
   {"id": 2, "name": "Илья"},
 ]
+let LAST_ID = 2;
 router.get('/', function(req, res, next) {
   res.send('Hello, world!');
 });
+
+router.post('/', function(req, res) {
+  LAST_ID++;
+  const newUser = {"id": LAST_ID, "name": req.body.name};
+  items.push(req.body);
+  res.status(201).json(newUser);
+})
 
 module.exports = router;
