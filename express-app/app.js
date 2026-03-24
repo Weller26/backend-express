f17e2dc1607a95551b19729280b587943891f205
Алсу Исламова <161420770+mrrled@users.noreply.github.com>
2026-03-24 17:24:35 +0500

    6 task

--- express-app/app.js
+++ express-app/app.js
@@ -8,11 +8,18 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use((req, res, next) => {
+    if (req.query["auth"] !== "true") {
+        return res.sendStatus(401);
+    }
+    return next();
+});
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
