1d7ef550d0c09e229d6f058d42d4d51b0097d89e
Weller26 <Ya.zloy246@gmail.com>
2026-03-24 16:57:37 +0500

    Add items

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+
+items = [
+  {"id": 1, "name": "Алсу"},
+  {"id": 2, "name": "Илья"},
+]
 router.get('/', function(req, res, next) {
   res.send('Hello, world!');
 });
