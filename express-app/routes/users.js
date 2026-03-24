2f7bcd1099e7a5ed1c231daf216d0c6e25087457
Weller26 <Ya.zloy246@gmail.com>
2026-03-24 17:11:27 +0500

    6 done

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -11,4 +11,14 @@ router.get('/', function(req, res, next) {
   res.status(200).json(items);
 });
 
+router.get('/:id', function(req, res, next) {
+  const targetId = Number(req.params.id);
+  const user = items.filter(item => item.id === targetId);
+  if (user.length === 0) {
+    res.status(404).json({"message": "User not found"});
+  } else {
+    res.status(200).json(user);
+  }
+})
+
 module.exports = router;
